877b07c5a7ef5f78055d1c61a9614516c8371d6e
Arjun <arjunjot18@gmail.com>
2023-09-25 16:42:36 +0200

    graph_added

--- src/components/TempGraph.vue
+++ src/components/TempGraph.vue
@@ -25,33 +25,7 @@
       <label class="self-center">{{ formatTimeFromDate(item.time) }}</label>
     </div>
   </div>
-</div>
-
-  <!-- <div class="text-white w-full overflow-hidden overflow-x-scroll">
-      <div class="flex flex-row h-auto w-[850px] mr-4">
-        <vue-custom-scrollbar
-    class="scroll-area"
-    :settings="settings"
-    @ps-scroll-y="scrollHanle"
-    >
-        <div
-          v-for="(item, index) in this.data.forecast.forecastday[0].hour"
-          :key="index"
-          class=""
-        >
-          <div class="flex flex-col w-[200px]">
-            <label class="text-6xl font-light self-start"
-              >{{ item.temp_c }}ºC</label
-            >
-            <label class="self-start">{{
-              formatTimeFromDate(item.time)
-            }}</label>
-          </div>
-        </div>
-  </vue-custom-scrollbar>
-
-      </div>
-    </div> -->
+</div> 
 </template>
 <script> 
 import vueCustomScrollbar from "vue-custom-scrollbar/src/vue-scrollbar.vue";
@@ -63,10 +37,7 @@ export default {
     data: Object, // this.data.forecast.forecastday[0].hour
   },
 
-  methods: {
-    scrollHanle(evt) {
-      console.log(evt);
-    },
+  methods: { 
     formatTimeFromDate(dateString) {
       // Parse the date string into a Date object
       const date = new Date(dateString);
