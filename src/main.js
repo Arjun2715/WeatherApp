393aa02782b03541242876c8e0615bbd0af9e347
Arjun2715 <arjunjot18@gmail.com>
2025-01-12 13:55:26 +0100

    fixes

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
 import "./input.css";
-
 const app = createApp(App);
 app.use(store);
 app.mount("#app");
-
 store.dispatch("initializeStoredStrings"); // Initialize the stored strings from LocalStorage
