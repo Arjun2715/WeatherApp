bd3534ddc541897aabd163ef38eff30167710537
Arjun2715 <arjunjot18@gmail.com>
2025-01-24 13:59:03 +0100

    fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-    {
-        path: "/",
-        name: "/",
-        redirect: "/dashboard",
-      },
-    
-]
+  {
+    path: "/",
+    name: "/",
+    redirect: "/dashboard",
+  },
+];
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
-    linkActiveClass: "active",
-  });
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+  linkActiveClass: "active",
+});
 export default router;
